a5cfa8184edacdb539483bb90a1a224554835d37
Kingmoha123 <154725193+Kingmoha123@users.noreply.github.com>
2026-02-08 17:08:40 +0300

    Update MongoDB connection string to use Atlas

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(express.json())
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/sms")
+  .connect(process.env.MONGODB_URI || "mongodb+srv://mohan21:123@cluster0.afaipw7.mongodb.net/?appName=Cluster0")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err))
 
